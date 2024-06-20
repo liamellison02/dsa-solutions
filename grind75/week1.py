a5ab04d58325e5f6392a1ebef07565b60e388f57
Liam Ellison <liam.ellison.codes@gmail.com>
2024-06-19 21:15:01 -0400

    solved LC#21

--- grind75/week1.py
+++ grind75/week1.py
@@ -1,6 +1,11 @@
 import collections
-from typing import List, Dict, Tuple, Any, Union
+from typing import List, Dict, Tuple, Any, Union, Optional
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     
@@ -35,3 +40,27 @@ class Solution:
                 return False
 
         return True if len(stack) == 0 else False
+
+    # 3. Merge Two Sorted Lists
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        head = ListNode()
+        tail = head
+
+        while list1 and list2:
+
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+        
+        return head.next
