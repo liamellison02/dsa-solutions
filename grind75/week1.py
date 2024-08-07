aac3a21abf978c46a2e6e37f10f174789ea1c90e
liamellison02 <liam.ellison.codes@gmail.com>
2024-08-06 23:11:48 -0400

    Solved LC#3

--- grind75/week1.py
+++ grind75/week1.py
@@ -295,6 +295,19 @@ class Solution:
         
         return retArr
     
+    # 17. Longest Substring Without Repeating Characters
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        unique, longest, lb = set(), 0, 0
+    
+        for rb in range(len(s)):
+            while s[rb] in unique:
+                unique.remove(s[lb])
+                lb += 1
+            unique.add(s[rb])
+            longest = max(longest, rb-lb+1)
+
+        return longest
 
 
 if __name__ == '__main__':
