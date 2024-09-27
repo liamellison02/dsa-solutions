35d488f4995c22bf8350a99f59ff1dd0e723c818
Liam Ellison <liam.ellison.codes@gmail.com>
2024-09-26 22:09:15 -0400

    new solutions
    1. largest X figure in matrix.
    2. peak amount of meetings given meeting time intervals

--- grind75/stuff.py
+++ grind75/stuff.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def findPairs(numbers):
     arrLen = len(numbers)
     i, j, numPairs = 0, 0, 0
@@ -52,17 +54,92 @@ def findPairs(numbers):
 # given an array of meeting intervals, where arr[i][0] = startHour and 
 # arr[i][1] = endHour (not inclusive), return the peak number of meetings in an hour
 
+def peakMeetings(intervals: list):
+    times = []
+    for i in intervals:
+        times.append((i[0], "start"))
+        times.append((i[1], "stop"))
+    
+    times.sort()
+    
+    maxMeetings = 0
+    currMeetings = 0
+    latestTime = -1
+    
+    for time, type in times:
+        if time > latestTime:
+            if currMeetings > maxMeetings:
+                maxMeetings = currMeetings
+        latestTime = time
+        
+        if type == "start":
+            currMeetings += 1
+        else:
+            currMeetings -= 1
+
+    if currMeetings > maxMeetings:
+        maxMeetings = currMeetings
+    
+    return maxMeetings
+    
+
 def largestXFigure(matrix):
     # function to find the largest x-figure in a matrix
     m = len(matrix)
     n = len(matrix[0])
-    l, r, top, bottom = 1, n-2, 0, 0
+    largestX = (0,0)
+    largestXLen = 0
+    a = [[0 for _ in range(n)] for _ in range(m)]
+    b = [[0 for _ in range(n)] for _ in range(m)]
+    c = [[0 for _ in range(n)] for _ in range(m)]
+    d = [[0 for _ in range(n)] for _ in range(m)]
     
-    # maxLen = 0
-    # upDiags, downDiags = [], []
-
-    # notUp = (-1, -1)
-    # notDown = (-1, -1)
+    # fill in botLeftTopRight:
+    for row in range(m-1, -1, -1):
+        for col in range(n-1, -1, -1):
+            val = matrix[row][col]
+            if col == 0 or row == m-1 or val == 0:
+                a[row][col] = val
+            else:
+                a[row][col] = val + a[row+1][col-1]
+    
+    # fill in botRightTopLeft:
+    for row in range(m-1, -1, -1):
+        for col in range(n):
+            val = matrix[row][col]
+            if row == m-1 or col == n-1 or val == 0:
+                b[row][col] = val
+            else:
+                b[row][col] = val + b[row+1][col+1]
+                
+    # fill in topLeftBotRight:
+    for row in range(m):
+        for col in range(n-1, -1, -1):
+            val = matrix[row][col]
+            if row == 0 or col == 0 or val == 0:
+                c[row][col] = val
+            else:
+                c[row][col] = val + c[row-1][col-1]
+                
+    # fill in topRightBotLeft:
+    for row in range(m):
+        for col in range(n):
+            val = matrix[row][col]
+            if row == 0 or col == n-1 or val == 0:
+                d[row][col] = val
+            else:
+                d[row][col] = val + d[row-1][col+1]
+    
+    
+    for row in range(m):
+        for col in range(n):
+            if min(a[row][col], b[row][col], c[row][col], d[row][col]) > largestXLen:
+                largestXLen = min(a[row][col], b[row][col], c[row][col], d[row][col])
+                largestX = (row, col)
+    
+    return largestX, largestXLen
+                    
+            
 
     # for row in range(m):
     #     for col in range(n):
@@ -93,4 +170,11 @@ def largestXFigure(matrix):
     #                 else:
     #                     break
     #             maxLen = max(maxLen, currLen)
-    return maxLen
+
+if __name__ == "__main__":
+    print(peakMeetings([[1, 5], [2, 4], [4, 5], [4, 6], [2, 7], [6,9]])) # 4
+    print(largestXFigure([[1, 0, 1, 0, 1], 
+                          [0, 1, 0, 1, 0], 
+                          [1, 0, 1, 0, 1],
+                          [0, 1, 0, 1, 0], 
+                          [1, 0, 1, 0, 1]])) # ((2, 2), 3)
