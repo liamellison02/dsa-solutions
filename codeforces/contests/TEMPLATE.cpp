c8e1b20e41bc4888cb3ce419c675e1a3769890a5
Liam Ellison <liam.word@gmail.com>
2025-08-30 22:18:17 -0400

    added a basic template for contests... updates to come

---
+++ codeforces/contests/TEMPLATE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+void solution() {
+    return;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solution();
+    return 0;
+}
