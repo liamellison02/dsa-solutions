fa844dfcbe34515cde09f1b272a0159415ff7c72
Liam Ellison <liam.ellison.codes@gmail.com>
2025-12-21 13:19:09 -0500

    solved LC#332 and LC#743 - dijkstra's & hierholzer's algorithms

---
+++ problems/graphs/shortest_path/lc-743-network-delay-time.cpp
@@ -0,0 +1,56 @@
+/*
+platform: lc
+id: 743
+name: network delay time
+pattern: graphs/shortest_path
+tags: dijkstras,heap
+complexity: O(ElogE) time, O(E) space : E = # of edges
+notes: simple dijkstras problem, solution likely has room for
+micro-optimizations
+*/
+
+#include <queue>
+#include <vector>
+using namespace std;
+using VI = vector<int>;
+using PII = pair<int, int>;
+using VPI = vector<PII>;
+using VVPI = vector<VPI>;
+
+class Solution {
+public:
+  int networkDelayTime(vector<vector<int>> &times, int n, int k) {
+    VVPI adj(n + 1);
+    VI dist(n + 1, -1);
+
+    for (VI &t : times)
+      adj[t[0]].push_back({t[1], t[2]});
+
+    priority_queue<PII, VPI, greater<PII>> minh;
+
+    minh.push({0, k});
+    while (!minh.empty()) {
+      auto [d, v] = minh.top();
+      minh.pop();
+
+      if (dist[v] != -1)
+        continue;
+
+      dist[v] = d;
+
+      for (auto &[n, w] : adj[v]) {
+        if (dist[n] == -1)
+          minh.push({d + w, n});
+      }
+    }
+
+    int tf = 0;
+    for (int i = 1; i <= n; ++i) {
+      if (dist[i] == -1)
+        return -1;
+      tf = max(tf, dist[i]);
+    }
+
+    return tf;
+  }
+};
---
+++ problems/graphs/traversal/lc-332-reconstruct-itinerary.cpp
@@ -0,0 +1,45 @@
+/*
+platform: lc
+id: 332
+name: reconstruct itinerary
+pattern: graphs/traversal
+tags: dfs, hierholzer, eulerian path/circuit
+complexity:
+notes: eulerian path finding problem (path using all edges once)
+*/
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using VS = vector<string>;
+
+class Solution {
+public:
+  vector<string> findItinerary(vector<vector<string>> &tickets) {
+    unordered_map<string, VS> adj;
+
+    for (VS &t : tickets)
+      adj[t[0]].push_back(t[1]);
+    for (auto &[src, dest] : adj)
+      sort(dest.rbegin(), dest.rend());
+
+    VS res, stk;
+
+    stk.push_back("JFK");
+    while (!stk.empty()) {
+      string curr = stk.back();
+      if (adj[curr].empty()) {
+        res.push_back(curr);
+        stk.pop_back();
+      } else {
+        stk.push_back(adj[curr].back());
+        adj[curr].pop_back();
+      }
+    }
+
+    reverse(res.begin(), res.end());
+    return res;
+  }
+};
