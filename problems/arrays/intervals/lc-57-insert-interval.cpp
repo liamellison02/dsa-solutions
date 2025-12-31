e6f31b2ced6e82b43a6e85bd6a3b021f64c9da1e
Liam Ellison <liam.word@gmail.com>
2025-12-31 16:43:41 -0500

    solved LC 57

--- problems/arrays/intervals/lc-57-insert-interval.cpp
+++ problems/arrays/intervals/lc-57-insert-interval.cpp
@@ -3,11 +3,12 @@ platform: lc
 id: 57
 name: insert interval
 pattern: arrays/intervals
-tags: intervals,arrays
+tags: intervals,array
 complexity:
-- time = O()
-- space = O()
-notes:
+- time = O(n)
+- space = O(n)
+notes: iterate and merge overlap into new interval; once past merging, append
+remainder
 */
 
 #include <algorithm>
@@ -32,23 +33,24 @@ using VTI = vector<TI>;
 
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        VVI res;
-        int n = intervals.size(), start = newInterval[0], end = newInterval[1];
+  vector<vector<int>> insert(vector<vector<int>> &intervals,
+                             vector<int> &newInterval) {
+    VVI res;
+    int n = intervals.size(), start = newInterval[0], end = newInterval[1];
 
-        for (int i{}; i<n; ++i) {
-            if (intervals[i][0] > end) {
-                res.push_back(newInterval);
-                copy(intervals.begin()+i, intervals.end(), back_inserter(res));
-                return res;
-            }
-            else if (intervals[i][1] < start) res.push_back(intervals[i]);
-            else {
-                newInterval[0] = min(newInterval[0], intervals[i][0]);
-                newInterval[1] = max(newInterval[1], intervals[i][1]);
-            }
-        }
+    for (int i{}; i < n; ++i) {
+      if (intervals[i][0] > end) {
         res.push_back(newInterval);
+        copy(intervals.begin() + i, intervals.end(), back_inserter(res));
         return res;
+      } else if (intervals[i][1] < start)
+        res.push_back(intervals[i]);
+      else {
+        newInterval[0] = min(newInterval[0], intervals[i][0]);
+        newInterval[1] = max(newInterval[1], intervals[i][1]);
+      }
     }
+    res.push_back(newInterval);
+    return res;
+  }
 };
