8950afd1812067de5bede5a568958dec5bc9a87a
Liam Ellison <liam.word@gmail.com>
2025-12-31 16:08:44 -0500

    solved LC 57

---
+++ problems/arrays/intervals/lc-57-insert-interval.cpp
@@ -0,0 +1,54 @@
+/*
+platform: lc
+id: 57
+name: insert interval
+pattern: arrays/intervals
+tags: intervals,arrays
+complexity:
+- time = O()
+- space = O()
+notes:
+*/
+
+#include <algorithm>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VI = vector<int>;
+using VVI = vector<VI>;
+using VC = vector<char>;
+using VVC = vector<VC>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        VVI res;
+        int n = intervals.size(), start = newInterval[0], end = newInterval[1];
+
+        for (int i{}; i<n; ++i) {
+            if (intervals[i][0] > end) {
+                res.push_back(newInterval);
+                copy(intervals.begin()+i, intervals.end(), back_inserter(res));
+                return res;
+            }
+            else if (intervals[i][1] < start) res.push_back(intervals[i]);
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+        res.push_back(newInterval);
+        return res;
+    }
+};
