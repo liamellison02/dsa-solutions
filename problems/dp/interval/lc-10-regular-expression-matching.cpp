9e9f0d1f6affa69d8e89dd471c189af117c466ba
Liam Ellison <liam.ellison.codes@gmail.com>
2026-01-24 17:56:09 -0500

    solved LC #10

---
+++ problems/dp/interval/lc-10-regular-expression-matching.cpp
@@ -0,0 +1,61 @@
+/*
+platform: lc
+id: 10
+name: regular expression matching
+pattern: dp/interval
+tags: dp,interval-dp,regex,2d-dp,string
+complexity:
+- time = O()
+- space = O()
+notes:
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VB = vector<bool>;
+using VC = vector<char>;
+using VI = vector<int>;
+using VS = vector<string>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+using VVC = vector<VC>;
+using VVI = vector<VI>;
+using VVS = vector<VS>;
+using VVPI = vector<VPI>;
+using SI = set<int>;
+using USI = unordered_set<int>;
+using MII = map<int, int>;
+using UMII = unordered_map<int, int>;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  // paste method here
+};
