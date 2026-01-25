ddde70249e0f55fad03383a520a6bb4cd555fade
Liam Ellison <liam.ellison.codes@gmail.com>
2026-01-24 20:36:19 -0500

    solved LC #1984 - 01/24 daily challenge

---
+++ problems/sliding_window/lc-1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,65 @@
+/*
+platform: lc
+id: 1984
+name: minimum difference between highest and lowest of k scores
+pattern: sliding_window
+tags: sorting,sliding-window,arrays
+complexity:
+- time = O(nlogn)
+- space = O(1)
+notes:
+just think abt it lol
+*/
+
+#include <algorithm>
+#include <climits>
+#include <map>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VB = vector<bool>;
+using VC = vector<char>;
+using VI = vector<int>;
+using VS = vector<string>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+using VVC = vector<VC>;
+using VVI = vector<VI>;
+using VVS = vector<VS>;
+using VVPI = vector<VPI>;
+using SI = set<int>;
+using USI = unordered_set<int>;
+using MII = map<int, int>;
+using UMII = unordered_map<int, int>;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int minimumDifference(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end());
+    int res{INT_MAX};
+    for (int i{}, j{k - 1}; j < nums.size(); ++i, ++j)
+      if (nums[j] - nums[i] < res)
+        res = nums[j] - nums[i];
+    return res;
+  }
+};
