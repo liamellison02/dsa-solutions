15f8b10b5ef1c18ace22e292744dae53cc4f7705
Liam Ellison <liam.ellison.codes@gmail.com>
2026-01-26 18:07:17 -0500

    solved LC #252

---
+++ problems/arrays/intervals/lc-252-meeting-rooms.cpp
@@ -0,0 +1,68 @@
+/*
+platform: lc
+id: 252
+name: meeting rooms
+pattern: arrays/intervals
+tags: intervals,sorting
+complexity:
+- time = O(nlogn)
+- space = O(logn)
+notes:
+cmon just think abt it gang
+*/
+
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VB = vector<bool>;
+using VC = vector<char>;
+using VI = vector<int>;
+using VS = vector<string>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+using VVC = vector<VC>;
+using VVI = vector<VI>;
+using VVS = vector<VS>;
+using VVPI = vector<VPI>;
+using SI = set<int>;
+using USI = unordered_set<int>;
+using MII = map<int, int>;
+using UMII = unordered_map<int, int>;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  bool canAttendMeetings(vector<vector<int>> &intervals) {
+    sort(intervals.begin(), intervals.end(),
+         [](vector<int> &a, vector<int> &b) { return a[0] < b[0]; });
+
+    int n = intervals.size();
+
+    for (int i{}; i < n - 1; ++i)
+      if (intervals[i][1] > intervals[i + 1][0])
+        return false;
+
+    return true;
+  }
+};
