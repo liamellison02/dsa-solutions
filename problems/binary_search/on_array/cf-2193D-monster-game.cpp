b5e3db6be9d3b7aef2fed68607b42c647c6d61a4
Liam Ellison <liam.ellison.codes@gmail.com>
2026-01-26 19:37:19 -0500

    solved CF 2193D + CF template update

--- problems/binary_search/on_array/cf-2193D-monster-game.cpp
+++ problems/binary_search/on_array/cf-2193D-monster-game.cpp
@@ -1,22 +1,21 @@
 /*
-platform:
-id:
-name:
-pattern:
-tags:
+platform: cf
+id: 2193D
+name: monster game
+pattern: binary_search/on_array
+tags: prefix-sum,sorting,binary-search,greedy
 complexity:
 notes:
+sort the swords and build prefix sum array for lvl costs.
+then do binary search on prefix sum arr for each sword to find find max
+clearable lvls w/ curr sword.
+return max score found for each sword strength in swords as the difficulty.
 */
 
 #include <algorithm>
-#include <climits>
-#include <cmath>
-#include <deque>
 #include <iostream>
 #include <map>
-#include <queue>
 #include <set>
-#include <stack>
 #include <string>
 #include <tuple>
 #include <unordered_map>
@@ -38,7 +37,6 @@ using VPI = vector<PI>;
 using VTI = vector<TI>;
 using VVC = vector<VC>;
 using VVI = vector<VI>;
-using VVll = vector<Vll>;
 using VVS = vector<VS>;
 using VVPI = vector<VPI>;
 using SI = set<int>;
@@ -57,7 +55,32 @@ struct TreeNode {
 };
 
 static inline void solve() {
-  // write solution here
+  int n;
+  cin >> n;
+  Vll s(n);
+  Vll l(n);
+  for (int i{0}; i < n; ++i)
+    cin >> s[i];
+  for (int i{0}; i < n; ++i)
+    cin >> l[i];
+
+  sort(s.begin(), s.end());
+
+  Vll pre(n + 1);
+  for (int i{}; i < n; ++i)
+    pre[i + 1] = pre[i] + l[i];
+
+  ll res{0};
+  for (int i{0}; i < n; ++i) {
+    ll swords = n - i;
+
+    int lvl = upper_bound(pre.begin(), pre.end(), swords) - pre.begin() - 1;
+
+    if ((s[i] * lvl) > res)
+      res = s[i] * lvl;
+  }
+
+  cout << res << '\n';
 }
 
 int main() {
