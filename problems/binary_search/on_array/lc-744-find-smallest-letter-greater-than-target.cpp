c3ec3cdcfe7cf8c6af2aa654936f8b6b5238d93a
Liam Ellison <liam.ellison.codes@gmail.com>
2026-02-01 14:29:36 -0500

    solved LC #744 - 01/31 daily challenge

---
+++ problems/binary_search/on_array/lc-744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,75 @@
+/*
+platform: lc
+id: 744
+name: find smallest letter greater than target
+pattern: binary_search/on_array
+tags: array,binary-search
+complexity:
+- time = O(logn)
+- space = O(1)
+notes:
+pretty straightforward. input is sorted so can use binary search here.
+*/
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+
+using ll = long long;
+using ull = unsigned long long;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VB = vector<bool>;
+using VC = vector<char>;
+using VI = vector<int>;
+using Vll = vector<ll>;
+using VS = vector<string>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+using VVC = vector<VC>;
+using VVI = vector<VI>;
+using VVll = vector<Vll>;
+using VVS = vector<VS>;
+using VVPI = vector<VPI>;
+using SI = set<int>;
+using USI = unordered_set<int>;
+using MII = map<int, int>;
+using UMII = unordered_map<int, int>;
+
+template <typename T> using vec = vector<T>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  char nextGreatestLetter(vector<char> &letters, char target) {
+    auto it = upper_bound(letters.begin(), letters.end(), target);
+    return it == letters.end() ? letters[0] : *it;
+  }
+};
