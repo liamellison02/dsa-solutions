cb89a74c3b39afab250ec8d47431b381d4cc7113
Liam Ellison <liam.ellison.codes@gmail.com>
2026-02-07 14:31:18 -0500

    solved LC #1653 - 02/07 daily challenge (happy bday livy, ilysm <3)

---
+++ problems/dp/linear/lc-1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,79 @@
+/*
+platform: lc
+id: 1653
+name: minimum deletions to make string balanced
+pattern: dp/linear
+tags: dp,bottom-up,greedy
+complexity:
+- time = O(n)
+- space = O(1)
+notes:
+optimized bottom-up dp:
+dp[i] = min deletions to balance s[0:i-1] (inclusive)
+*/
+
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define rep(i, n) for (int i{0}; i < (n); ++i)
+#define rrep(i, n) for (int i{(n) - 1}; i >= 0; --i)
+#define FOR(i, a, b) for (int i{(a)}; i < (b); ++i)
+
+using ll = long long;
+using ull = unsigned long long;
+using PI = pair<int, int>;
+using TI = tuple<int, int, int>;
+using VB = vector<bool>;
+using VC = vector<char>;
+using VI = vector<int>;
+using Vll = vector<ll>;
+using VS = vector<string>;
+using VPI = vector<PI>;
+using VTI = vector<TI>;
+using VVC = vector<VC>;
+using VVI = vector<VI>;
+using VVll = vector<Vll>;
+using VVS = vector<VS>;
+using VVPI = vector<VPI>;
+using SI = set<int>;
+using USI = unordered_set<int>;
+using MII = map<int, int>;
+using UMII = unordered_map<int, int>;
+
+template <typename T> using vec = vector<T>;
+template <typename T> using pq = priority_queue<T>;
+template <typename T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  int minimumDeletions(string s) {
+    int n = s.size(), b{0}, res{0};
+
+    for (int i{0}; i < n; ++i) {
+      if (s[i] == 'a')
+        res = (res < b) ? res + 1 : b;
+      else
+        ++b;
+    }
+
+    return res;
+  }
+};
