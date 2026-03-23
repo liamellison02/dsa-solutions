bdb8a106271478551fa33aed80d9145dd3cba2c8
Liam Ellison <liam.ellison.codes@gmail.com>
2026-03-23 15:20:45 -0400

    template updates

--- templates/cf.cpp
+++ templates/cf.cpp
@@ -25,30 +25,40 @@ notes:
 
 using namespace std;
 
+constexpr int MOD = 1e9 + 7;
+
 #define rep(i, n) for (int i{0}; i < (n); ++i)
 #define rrep(i, n) for (int i{(n) - 1}; i >= 0; --i)
 #define FOR(i, a, b) for (int i{(a)}; i < (b); ++i)
 
+using str = string;
+
 using ll = long long;
 using ull = unsigned long long;
-using PI = pair<int, int>;
-using TI = tuple<int, int, int>;
-using VB = vector<bool>;
-using VC = vector<char>;
-using VI = vector<int>;
-using Vll = vector<ll>;
-using VS = vector<string>;
-using VPI = vector<PI>;
-using VTI = vector<TI>;
-using VVC = vector<VC>;
-using VVI = vector<VI>;
-using VVll = vector<Vll>;
-using VVS = vector<VS>;
-using VVPI = vector<VPI>;
-using SI = set<int>;
-using USI = unordered_set<int>;
-using MII = map<int, int>;
-using UMII = unordered_map<int, int>;
+using ui8 = uint8_t;
+using ui16 = uint16_t;
+using ui32 = uint32_t;
+using ui64 = uint64_t;
+
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ti = tuple<int, int, int>;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vs = vector<string>;
+using vpi = vector<pi>;
+using vti = vector<ti>;
+using vvc = vector<vc>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vvs = vector<vs>;
+using vvpi = vector<vpi>;
+using si = set<int>;
+using usi = unordered_set<int>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
 
 template <typename T> using vec = vector<T>;
 template <typename T> using pq = priority_queue<T>;
